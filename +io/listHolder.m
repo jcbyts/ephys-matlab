231c0c16911294a2a8c6bc14c4a0c2584f41a513
Jake <jacoby8s@gmail.com>
2017-07-28 17:02:56 -0400

    Starting to build multi-session analysis

---
+++ +io/listHolder.m
@@ -0,0 +1,13 @@
+classdef listHolder < handle
+    % listHolder is an annoying way to track a variable through the use of callback
+    % functions
+    
+    properties
+        list
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ +io/selectSessions.m
@@ -0,0 +1,20 @@
+function [dataDir, val] = selectSessions
+
+dataDir = uigetdir(pwd, 'Select Data Directory');
+
+fl = dir(dataDir);
+fl(1:2) = []; % remove relative directories
+fl(~arrayfun(@(x) x.isdir, fl)) = []; % remove non-directory items
+
+l = io.listHolder;
+
+fig = uifigure('Name', 'Select Sessions. Close figure to continue', 'Position', [100 500 350 400]); % 'CloseRequestFcn', @(fig, event) myclosereq(fig,lbx)
+lbx = uilistbox(fig, 'Position', [10 10 300 350], 'Items', {fl.name}, 'MultiSelect', 'on', 'ValueChangedFcn', @(lbx, event) selectionChanged(lbx,l)); %
+
+uiwait(fig)
+val = l.list;
+% 
+function selectionChanged(lbx,l)
+l.list = lbx.Value;
+
+
