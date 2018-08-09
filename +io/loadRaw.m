ae68e6c6857a2b667466e45f7f1fefb2c4afac3c
Jake <jacoby8s@gmail.com>
2018-08-09 12:12:38 -0400

    load Raw can load the high pass filtered data if it exists and is requested

--- +io/loadRaw.m
+++ +io/loadRaw.m
@@ -1,11 +1,19 @@
-function [data, timestamps] = loadRaw(ops, inds, inVolts)
-% [data, timestamps] = loadRaw(ops, inds)
+function [data, timestamps] = loadRaw(ops, inds, inVolts, fproc)
+% [data, timestamps] = loadRaw(ops, inds, inVolts, fproc)
+
+if nargin < 4
+    fproc = false;
+end
 
 if nargin < 3
     inVolts = false;
 end
 
-fid = fopen(ops.fbinary);
+if fproc
+    fid = fopen(ops.fproc);
+else
+    fid = fopen(ops.fbinary);
+end
 
 fseek(fid, 0, 'eof');
 filesize = ftell(fid);
@@ -23,13 +31,22 @@ else
     bufferSize = [Nchan inds(end)-(inds(1)-1)];
 end
 
-data = double(fread(fid, bufferSize, '*int16'));
+data = fread(fid, bufferSize, '*int16');
 
 if nargout > 1 || inVolts
    
     info = load(fullfile(ops.root, 'ephys_info.mat'));
     if inVolts
         data = double(data)*info.bitVolts;
+        
+        
+%         if isfield(info, 'artifacts')
+%             for iCh = 1:size(data,1)
+%                 artifacts = info.artifacts - inds(1);
+%                 artifacts = artifacts(artifacts > 0 & artifacts < (inds(end) - inds(1)));
+%                 data(iCh,artifacts) = nan;
+%             end
+%         end
     end
     
     timestamps = io.convertSamplesToTime(inds, info.sampleRate, info.timestamps(:), info.fragments(:));
