eac4ba2fbae491acca96e4b0863811a779a2f737
Jacob Yates <jacoby8s@gmail.com>
2018-09-20 09:29:20 -0400

    findPDScontainingStimModule is streamlined, but it requires pldaps repo now.

--- +io/findPDScontainingStimModule.m
+++ +io/findPDScontainingStimModule.m
@@ -11,20 +11,10 @@ function hasStim = findPDScontainingStimModule(PDS, stim)
 
 hasStim = false(numel(PDS),1);
 for i = 1:numel(PDS)
-    if isfield(PDS{i}.initialParametersMerged, stim)
-        ixcond = cellfun(@(x) isfield(x, stim), PDS{i}.conditions);
-        ixdata = cellfun(@(x) isfield(x, stim), PDS{i}.data);
-        if any(ixcond)
-            ixuse = cellfun(@(x) isfield(x.(stim), 'use'), PDS{i}.conditions(ixcond));
-            inds  = find(ixcond);
-            hasStim(i) = any(cellfun(@(x) x.(stim).use, PDS{i}.conditions(inds(ixuse)))) | PDS{i}.initialParametersMerged.(stim).use;
-        elseif any(ixdata)
-            ixuse = cellfun(@(x) isfield(x.(stim), 'use'), PDS{i}.data(ixdata));
-            inds  = find(ixdata);
-            if isempty(inds) || ~any(ixuse)
-                continue
-            end
-            hasStim(i) = any(cellfun(@(x) x.(stim).use, PDS{i}.conditions(inds(ixuse)))) | PDS{i}.initialParametersMerged.(stim).use;
-        end
+    
+    trial = pds.getPdsTrialData(PDS);
+    
+    if isfield(trial, stim)
+        hasStim(i) = any(arrayfun(@(x) x.(stim).use, trial));
     end
 end
